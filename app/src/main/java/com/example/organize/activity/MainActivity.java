905b98383d8b55fc0f05f41f003fcc8965243a27
TiagoAndreAlmeida <thyagoeap@gmail.com>
2022-05-08 09:39:09 -0300

    add style and colors theme for signup and signin activity

--- app/src/main/java/com/example/organize/activity/MainActivity.java
+++ app/src/main/java/com/example/organize/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.organize.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.organize.R;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
@@ -37,5 +39,13 @@ public class MainActivity extends IntroActivity {
                 .fragment(R.layout.slide_intro)
                 .canGoForward(false)
                 .build());
+    }
+
+    public void onEntrar(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void onCadastrar(View view) {
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
